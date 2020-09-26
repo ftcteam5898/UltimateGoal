ed962eb381eaa495dbbeadd5ad27ec06fd959ace
ftcteam5898 <34162168+ftcteam5898@users.noreply.github.com>
2020-09-26 12:48:58 -0400

    Rudimentary forward created
    just added some functions, will optimize auto to accommodate angular movement and spins later.

--- FirstAuto.java
+++ FirstAuto.java
@@ -1,8 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
-import com.qualcomm.hardware.bosch.BNO055IMU;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
@@ -10,23 +9,115 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 public class FirstAuto extends LinearOpMode {
 
-    // todo: write your code here
-    private DcMotor lb, lf, rb, rf;
-    
-    private BNO055IMU imu2;
-    private Orientation angles2;
-    
-    private String position;
-    
-    private double headingO;
-    
     @Override
     public void runOpMode() {
-        
+
         setupHardware();
+        waitForStart();
+
+        //All right, let's see what we can do!
         setStopMode(DcMotor.ZeroPowerBehavior.BRAKE);
-        heading0 = getHeading();
-        // heading0 += 180;
-        while (!isStarted())
+        forward(1000, 0.1, 0.5, 0.65, 0.15, 0.333, 0.333);
+
+        while (opModeIsActive()) {}
+    }
+
+    public void setupHardware() {
+
+        //Initialize hardware
+        lb = hardwareMap.get(DcMotor.class, "lb");
+        lf = hardwareMap.get(DcMotor.class, "lf");
+        rb = hardwareMap.get(DcMotor.class, "rb");
+        rf = hardwareMap.get(DcMotor.class, "rf");
+
+    }
+
+    public void stopRobot() {
+
+        //Stop all motors
+        rf.setPower(0);
+        rb.setPower(0);
+        lf.setPower(0);
+        lb.setPower(0);
+
+    }
+
+    public void setRunMode(DcMotor.RunMode runMode) {
+
+        //Change all motor run modes to runMode variable
+        lb.setMode(runMode);
+        lf.setMode(runMode);
+        rb.setMode(runMode);
+        rf.setMode(runMode);
+
+    }
+
+    public void setStopMode(DcMotor.ZeroPowerBehavior zpb) {
+
+        //Change all motors to a ZeroPowerBehavior
+        lb.setZeroPowerBehavior(zpb);
+        lf.setZeroPowerBehavior(zpb);
+        rb.setZeroPowerBehavior(zpb);
+        rf.setZeroPowerBehavior(zpb);
+
+    }
+    
+    public void forward(int amt, double edge1, double edge2, double max, double min, double p1, double p2) {
+        
+        rf.setTargetPosition(rf.getCurrentPosition() + amt);
+        rb.setTargetPosition(rf.getCurrentPosition() + amt);
+        lf.setTargetPosition(rf.getCurrentPosition() - amt);
+        lb.setTargetPosition(rf.getCurrentPosition() - amt);
+        
+        setRunMode(DcMotor.RunMode.RUN_TO_POSITION);
+        
+        while ((rf.isBusy() && lf.isBusy() && rb.isBusy() && lb.isBusy()) && opModeIsActive()) {
+            
+            double prop = (amt - (rf.getTargetPosition() - rf.getCurrentPosition())) / (double) amt;
+            telemetry.addData("P", prop);
+            telemetry.update();
+            double adjPower = getPower(prop, edge1, edge2, max, min, p1, p2);
+            rf.setPower(adjPower);
+            lf.setPower(adjPower);
+            rb.setPower(adjPower);
+            lb.setPower(adjPower);
+            
+        }
+        
+        stopRobot();
+        
+        setRunMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+    
+    public double getPower(double amtDone, double edge1, double edge2, double max, double min, double p1, double p2) {
+        
+        if (amtDone >= edge1 && amtDone <= 1 - edge2) {
+            
+            return max;
+            
+        } else {
+            
+            if (amtDone > 1 - edge2) {
+                
+                amtDone = 1 - amtDone;
+                double amtNormDone = amtDone / edge2;
+                return min + evaluateNormal(1, p2, amtNormDone, max - min);
+                
+            } else {
+                
+                double amtNormDone = amtDone / edge1;
+                return min + evaluateNormal(1, p1, amtNormDone, max - min)
+                
+            }
+            
+        }
+        
+    }
+    
+    public double evaluateNormal(double m, double s, double x, double max) {
+        
+        double power = -Math.pow(x - m, 2) / (2 * Math.pow(s, 2));
+        return max * Math.pow(Math.e, power)
+        
     }
 }
