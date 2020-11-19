13c749e5584eaecdb3087a91f9bc66c204e70ace
Garv Gaur <59773926+toogiii@users.noreply.github.com>
2020-11-19 11:45:59 -0500

    Update OpenCVAuto.java

--- OpenCVAuto.java
+++ OpenCVAuto.java
@@ -1,9 +1,10 @@
-package org.firstinspires.ftc.teamcode.vision;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
@@ -13,7 +14,6 @@ import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
-import org.openftc.easyopencv.OpenCvInternalCamera;
 import org.openftc.easyopencv.OpenCvPipeline;
 
 @Autonomous(name = "OpenCVAuto", group = "Autonomous")
@@ -21,6 +21,7 @@ import org.openftc.easyopencv.OpenCvPipeline;
 public class OpenCVAuto extends LinearOpMode
 {
     OpenCvCamera webcam;
+    private DcMotor lb, lf, rb, rf;
     SkystoneDeterminationPipeline pipeline;
 
     @Override
@@ -35,7 +36,6 @@ public class OpenCVAuto extends LinearOpMode
         // We set the viewport policy to optimized view so the preview doesn't appear 90 deg
         // out when the RC activity is in portrait. We do our actual image processing assuming
         // landscape orientation, though.
-        webcam.setViewportRenderingPolicy(OpenCvCamera.ViewportRenderingPolicy.OPTIMIZE_VIEW);
 
         webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
         {
@@ -56,6 +56,8 @@ public class OpenCVAuto extends LinearOpMode
             telemetry.addData("Analysis", pipeline.getAnalysis());
             telemetry.addData("Position", pipeline.position);
             telemetry.update();
+
+
 
             // Don't burn CPU cycles busy-looping in this sample
             sleep(50);
@@ -164,5 +166,186 @@ public class OpenCVAuto extends LinearOpMode
         {
             return avg1;
         }
+
+
+    }
+    public void setupHardware() {
+
+        //Initialize hardware
+        lb = hardwareMap.get(DcMotor.class, "lb");
+        lf = hardwareMap.get(DcMotor.class, "lf");
+        rb = hardwareMap.get(DcMotor.class, "rb");
+        rf = hardwareMap.get(DcMotor.class, "rf");
+
+    }
+
+    public void stopRobot() {
+
+        //Stop all motors
+        rf.setPower(0);
+        rb.setPower(0);
+        lf.setPower(0);
+        lb.setPower(0);
+
+    }
+
+    public void setRunMode(DcMotor.RunMode runMode) {
+
+        //Change all motor run modes to runMode variable
+        lb.setMode(runMode);
+        lf.setMode(runMode);
+        rb.setMode(runMode);
+        rf.setMode(runMode);
+
+    }
+
+    public void setStopMode(DcMotor.ZeroPowerBehavior zpb) {
+
+        //Change all motors to a ZeroPowerBehavior
+        lb.setZeroPowerBehavior(zpb);
+        lf.setZeroPowerBehavior(zpb);
+        rb.setZeroPowerBehavior(zpb);
+        rf.setZeroPowerBehavior(zpb);
+
+    }
+
+    public void forward(int amt, double edge1, double edge2, double max, double min, double p1, double p2) {
+
+        rf.setTargetPosition(rf.getCurrentPosition() + amt);
+        rb.setTargetPosition(rb.getCurrentPosition() + amt);
+        lf.setTargetPosition(lf.getCurrentPosition() - amt);
+        lb.setTargetPosition(lb.getCurrentPosition() - amt);
+
+        setRunMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        while ((rf.isBusy() && lf.isBusy() && rb.isBusy() && lb.isBusy()) && opModeIsActive()) {
+
+            double prop = (amt - (rf.getTargetPosition() - rf.getCurrentPosition())) / (double) amt;
+            telemetry.addData("P", prop);
+            telemetry.update();
+            double adjPower = getPower(prop, edge1, edge2, max, min, p1, p2);
+            rf.setPower(adjPower);
+            lf.setPower(adjPower);
+            rb.setPower(adjPower);
+            lb.setPower(adjPower);
+
+        }
+
+        stopRobot();
+
+        setRunMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void backward(int amt, double edge1, double edge2, double max, double min, double p1, double p2) {
+
+        rf.setTargetPosition(rf.getCurrentPosition() - amt);
+        rb.setTargetPosition(rb.getCurrentPosition() - amt);
+        lf.setTargetPosition(lf.getCurrentPosition() + amt);
+        lb.setTargetPosition(lb.getCurrentPosition() + amt);
+
+        setRunMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        while ((rf.isBusy() && lf.isBusy() && rb.isBusy() && lb.isBusy()) && opModeIsActive()) {
+
+            double prop = (amt - (rf.getTargetPosition() - rf.getCurrentPosition())) / (double) amt;
+            telemetry.addData("P", prop);
+            telemetry.update();
+            double adjPower = getPower(prop, edge1, edge2, max, min, p1, p2);
+            rf.setPower(adjPower);
+            lf.setPower(adjPower);
+            rb.setPower(adjPower);
+            lb.setPower(adjPower);
+
+        }
+
+        stopRobot();
+
+        setRunMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void right(int amt, double edge1, double edge2, double max, double min, double p1, double p2) {
+
+        rf.setTargetPosition(rf.getCurrentPosition() - amt);
+        rb.setTargetPosition(rb.getCurrentPosition() + amt);
+        lf.setTargetPosition(lf.getCurrentPosition() - amt);
+        lb.setTargetPosition(lb.getCurrentPosition() + amt);
+
+        setRunMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        while ((rf.isBusy() && lf.isBusy() && rb.isBusy() && lb.isBusy()) && opModeIsActive()) {
+
+            double prop = (amt - (rf.getTargetPosition() - rf.getCurrentPosition())) / (double) amt;
+            telemetry.addData("P", prop);
+            telemetry.update();
+            double adjPower = getPower(prop, edge1, edge2, max, min, p1, p2);
+            rf.setPower(adjPower);
+            lf.setPower(adjPower);
+            rb.setPower(adjPower);
+            lb.setPower(adjPower);
+
+        }
+
+        stopRobot();
+
+        setRunMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void left(int amt, double edge1, double edge2, double max, double min, double p1, double p2) {
+
+        rf.setTargetPosition(rf.getCurrentPosition() + amt);
+        rb.setTargetPosition(rb.getCurrentPosition() - amt);
+        lf.setTargetPosition(lf.getCurrentPosition() + amt);
+        lb.setTargetPosition(lb.getCurrentPosition() - amt);
+
+        setRunMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        while ((rf.isBusy() && lf.isBusy() && rb.isBusy() && lb.isBusy()) && opModeIsActive()) {
+
+            double prop = (amt - (rf.getTargetPosition() - rf.getCurrentPosition())) / (double) amt;
+            telemetry.addData("P", prop);
+            telemetry.update();
+            double adjPower = getPower(prop, edge1, edge2, max, min, p1, p2);
+            rf.setPower(adjPower);
+            lf.setPower(adjPower);
+            rb.setPower(adjPower);
+            lb.setPower(adjPower);
+
+        }
+
+        stopRobot();
+
+        setRunMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public double getPower(double amtDone, double edge1, double edge2, double max, double min, double p1, double p2) {
+
+        if (amtDone >= edge1 && amtDone <= 1 - edge2) {
+
+            return max;
+
+        } else {
+
+            if (amtDone > 1 - edge2) {
+
+                amtDone = 1 - amtDone;
+                double amtNormDone = amtDone / edge2;
+                return min + evaluateNormal(1, p2, amtNormDone, max - min);
+
+            } else {
+
+                double amtNormDone = amtDone / edge1;
+                return min + evaluateNormal(1, p1, amtNormDone, max - min);
+
+            }
+
+        }
+
+    }
+
+    public double evaluateNormal(double m, double s, double x, double max) {
+
+        double power = -Math.pow(x - m, 2) / (2 * Math.pow(s, 2));
+        return max * Math.pow(Math.E, power);
+
     }
 }
